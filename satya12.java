53efcca6d65a17d688da6c148b0ecaa9848e8e14
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 06:05:19 +0100

    Create satya12.java

---
+++ satya12.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n;
+		n=s.nextInt();
+		int str2=n+1;
+		System.out.println(str2);
+	}
+}
