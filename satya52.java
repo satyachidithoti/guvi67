4fcc91d579ebd73c8a8e7f5b5d59418a3b16272a
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 08:01:45 +0100

    Create satya52.java

---
+++ satya52.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		switch(n)
+		{
+			case 1:System.out.println("One");break;
+			case 2:System.out.println("Two");break;
+			case 3:System.out.println("Three");break;
+			case 4:System.out.println("Four");break;
+			case 5:System.out.println("Five");break;
+			case 6:System.out.println("Six");break;
+			case 7:System.out.println("Seven");break;
+			case 8:System.out.println("Eight");break;
+			case 9:System.out.println("Nine");break;
+			case 10:System.out.println("Ten");break;
+			default:System.out.print("Invalid");
+ 
+		}
+	}
+}
