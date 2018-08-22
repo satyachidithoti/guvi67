ad5edd8209dc18f883d17ce0a64bc56f771acf7a
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 05:57:24 +0100

    Create Satya10.java

---
+++ Satya10.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a[]={1,2,3,4,5,6,7,8,9,10};
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		if(n==1|n==2|n==3|n==4|n==5|n==6|n==7|n==8|n==9|n==10)
+		System.out.println("yes");
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
