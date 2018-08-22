286aef910123270b0e5c5d782d83459d9135f8e7
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 15:20:31 +0100

    Create set6_6.java

---
+++ set6_6.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int cnt=0;
+		if(n!=0)
+		{
+		while(n!=0)
+		{
+			cnt++;
+			n=n/10;
+		}
+ 
+		System.out.print(cnt);	
+		}
+		else
+		System.out.print("1");
+ 
+ 
+	}
+}
