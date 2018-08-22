ce5424d8e9471a0fe1589d53eb09b1cf2c333458
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 07:21:13 +0100

    Create satya17.java

---
+++ satya17.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		System.out.print(n+m);
+	}
+}
