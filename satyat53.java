c91c69206463d0d62a0caedd5cfe44b3e4506dc0
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 08:08:27 +0100

    Create satyat53.java

---
+++ satyat53.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();int sum=0;
+		while(n!=0)
+		{
+			int rem=n%10;
+			sum=sum+rem;
+			n=n/10;
+		}
+		System.out.println(sum);
+	}
+}
