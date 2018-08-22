95786ea57dacad90f4c16cc4bd5d5ab966a55ff2
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 15:23:41 +0100

    Create set6_7.java

---
+++ set6_7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int k=s.nextInt();
+		int sum=0;
+		for(int i=1;i<=k;i++)
+		sum=sum+i;
+		System.out.print(sum);
+ 
+ 
+	}
+}
