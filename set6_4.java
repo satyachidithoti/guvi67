a71ab60dae26428c1a919eb5ddc9be5fe39c3151
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 15:12:27 +0100

    Create set6_4.java

---
+++ set6_4.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int k=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		int cnt=0;
+		for(int j=0;j<n;j++)
+		{
+			if(a[j]==k)
+			cnt++;
+		}
+		if(cnt>0)
+		System.out.print(cnt);
+		else
+			System.out.print(cnt);
+ 
+	}
+}
